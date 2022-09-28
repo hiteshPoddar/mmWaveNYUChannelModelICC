44b6c2f7437e73b628c6adf36ea69204a5a45395
Hitesh <hp2304@nyu.edu>
2022-09-28 15:01:32 -0400

    nyu nlos umi

--- src/propagation/model/channel-condition-model.cc
+++ src/propagation/model/channel-condition-model.cc
@@ -902,7 +902,7 @@ NYUUMiChannelConditionModel::ComputePlos (Ptr<const MobilityModel> a,
     pLos = pow((22.0 / distance2D)*(1.0 - 22.0 / distance2D) + exp (-distance2D / 100.0),2);
   }
   NS_LOG_DEBUG(" NYU UMI channel Probabilty LOS: "<< pLos);
-  pLos = 1;
+  pLos = 0;
   return pLos;
 }
 
